be3043ee195d6579a6712ff36a018335340f1dfc
JayasriMagendiravarman <jayasrivarman15@gmail.com>
2025-02-22 13:53:17 +0530

    Add files via upload

--- Java Basic Problems/problem6.java
+++ Java Basic Problems/problem6.java
@@ -1,1 +1,41 @@
+//Write a program to take x and print multiples of x till 1000.
 
+//Input:
+
+//100
+
+//Expected Output:
+
+//100
+
+//200
+
+//300
+
+//400
+
+//500
+
+//600
+
+//700
+
+//800
+
+//900
+
+//1000
+import java.util.Scanner;
+class problem6{
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the input");
+        int x=scan.nextInt();
+        for(int i=x; i<=1000; i=i+100)
+        {
+          System.out.println(i);
+            
+        }
+
+    }
+}
